dfaf2ba3b04654a9038757756b6794da64d0e96e
Brittany Storoz <brittanystoroz@gmail.com>
2015-11-17 23:31:44 -0500

    Add tests for marky tool

---
+++ kitsune/sumo/static/sumo/js/tests/fixtures/mochaSimpleButtonData.js
@@ -0,0 +1,21 @@
+export default [{
+    name: 'Bold',
+    defaultText: 'bold text',
+    className: 'btn-bold'
+  }, {
+    name: 'Italic',
+    defaultText: 'italic text',
+    className: 'btn-italic'
+  }, {
+    name: 'Insert a link...',
+    defaultText: 'link text',
+    className: 'btn-link'
+  }, {
+    name: 'Numbered List',
+    defaultText: 'Numbered list item',
+    className: 'btn-ol',
+  }, {
+    name: 'Bulleted List',
+    defaultText: 'Bulleted list item',
+    className: 'btn-ul'
+  }];
---
+++ kitsune/sumo/static/sumo/js/tests/markytests.js
@@ -0,0 +1,123 @@
+import {default as mochaJsdom, rerequire} from 'mocha-jsdom';
+import {default as chai, expect} from 'chai';
+import React from 'react';
+import chaiLint from 'chai-lint';
+import sinon from 'sinon';
+
+import mochaK from './fixtures/mochaK.js';
+import mochaJquery from './fixtures/mochaJquery.js';
+import mochaGettext from './fixtures/mochaGettext.js';
+import mochaSimpleButtonData from './fixtures/mochaSimpleButtonData.js';
+
+chai.use(chaiLint);
+
+describe('marky', () => {
+  mochaJsdom({useEach: true});
+  mochaJquery();
+  mochaK();
+  mochaGettext();
+  /* globals window, document, $ */
+
+  describe('marky toolbar creation', () => {
+    let $markyToolbar, markyToolbar, $markyTextarea, markyTextarea;
+
+    beforeEach(() => {
+      rerequire('../markup.js');
+
+      let sandbox = (
+        <div id="sandbox">
+          <div className="editor-tools"></div>
+          <div>
+            <textarea id="editor-textarea"></textarea>
+          </div>
+        </div>
+      );
+      React.render(sandbox, document.body);
+
+      $markyToolbar = $('.editor-tools');
+      $markyTextarea = $('#editor-textarea');
+    });
+
+    afterEach(() => {
+      React.unmountComponentAtNode(document.body);
+    });
+
+    it('should create a simpleToolbar containing 5 button elements', () => {
+      window.Marky.createSimpleToolbar($markyToolbar, $markyTextarea);
+      expect($markyToolbar.children('button').length).to.equal(5);
+
+      /**
+        TO TEST:
+        1. media button
+        2. canned responses
+        3. private messaging
+      **/
+    });
+
+    it('should create a fullToolbar editor containing 9 button elements', () => {
+      window.Marky.createFullToolbar($markyToolbar, $markyTextarea);
+      expect($markyToolbar.children('button').length).to.equal(9);
+    });
+
+    it('should render simpleButtons with appropriate information', () => {
+      window.Marky.createSimpleToolbar($markyToolbar, $markyTextarea);
+      let buttons = $markyToolbar.children('button');
+
+      for (let [index, fixtureData] of mochaSimpleButtonData.entries()) {
+        let $currentButton = $(buttons[index]);
+        expect($currentButton.attr('title')).to.equal(fixtureData.name);
+        expect($currentButton.hasClass(fixtureData.className)).to.beTrue();
+      }
+
+    });
+
+    it('should bind a click event to simpleButtons', () => {
+      window.Marky.createSimpleToolbar($markyToolbar, $markyTextarea);
+      // let button = new window.Marky.SimpleButton(gettext('Bold'), "'''", "'''", gettext('bold text'),
+      //     'btn-bold');
+      let button = $markyToolbar.children('button')[0];
+      // button.bind($markyTextarea).node();
+
+      let clickSpy = sinon.spy(button, 'handleClick');
+      $(button).click();
+      expect(clickSpy.called).to.beTrue();
+    });
+  });
+
+
+  describe('separator', () => {
+      /**
+        TO TEST:
+        1. renders a span element with the class of 'separator' between buttons
+      **/
+  });
+
+
+  describe('link button', () => {
+      /**
+        TO TEST:
+      **/
+  });
+
+  describe('media button', () => {
+      /**
+        TO TEST:
+      **/
+  });
+
+
+
+  describe('canned responses button', () => {
+      /**
+        TO TEST:
+      **/
+  });
+
+
+
+  describe('quote button', () => {
+      /**
+        TO TEST:
+      **/
+  });
+});
